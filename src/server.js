5dd4e5428749b6912df81bae41ae8f6660b123b5
Nugrawan <126413672+Nugrawan@users.noreply.github.com>
2023-07-08 08:20:38 +0700

    b

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 3000,
-    host: 'https://nugrawan-server.netlify.app',
+    host: 'https://nugrawan-server.netlify.app/notes',
     routes: {
       cors: {
         origin: ['*'],
